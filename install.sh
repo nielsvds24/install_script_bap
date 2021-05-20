9be97b5dec5d9d2f6fbd58da0b17114b58f485a1
Niels Van der Straeten <niels.vanderstraeten@student.odisee.be>
2021-05-20 19:12:22 +0200

    change install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt -y install apache2
 sudo apt -y install php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
-sudo tar -xvf install_script_bap/latest.tar.gz -C /var/www/html
+sudo tar -xvf install_script_bap/wordpress.tar.gz -C /var/www/html
 sudo chown -R www-data:www-data /var/www/html/wordpress
 sudo find /var/www/html/wordpress/ -type d -exec chmod 750 {} \;
 sudo find /var/www/html/wordpress/ -type f -exec chmod 640 {} \;
@@ -12,4 +12,3 @@ sudo mv install_script_bap/000-default.conf /etc/apache2/sites-available/000-def
 sudo a2enmod rewrite
 sudo apache2ctl configtest
 sudo systemctl restart apache2
-
