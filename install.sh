f2073b5885cd30e4d36458ac9a2468d47b5d173f
Niels Van der Straeten <niels.vanderstraeten@student.odisee.be>
2021-05-20 17:54:55 +0200

    change install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ sudo systemctl restart apache2
 sudo curl -o /var/www/html https://wordpress.org/latest.tar.gz
 sudo tar -xvf /var/www/html/latest.tar.gz
 sudo rm /var/www/html/latest.tar.gz
+sudo mv install_script_bap/wp-config.php /var/www/html/wordpress/wp-config.php
 sudo chown -R www-data:www-data /var/www/html/wordpress
 sudo find /var/www/html/wordpress/ -type d -exec chmod 750 {} \;
 sudo find /var/www/html/wordpress/ -type f -exec chmod 640 {} \;
-sudo mv install_script_bap/wp-config.php /var/www/html/wordpress/wp-config.php
-
+sudo rm -f /etc/apache2/sites-available/000-default.conf
+sudo mv install_script_bap/000-default.conf /etc/apache2/sites-available/000-default.conf
+sudo a2enmod rewrite
+sudo apache2ctl configtest
+sudo systemctl restart apache2
 
