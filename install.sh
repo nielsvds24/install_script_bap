aaea39c73035eae55302c937e0ab5ec568ce7de1
Niels Van der Straeten <niels.vanderstraeten@student.odisee.be>
2021-05-20 19:09:23 +0200

    change install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,7 @@
 sudo apt update
 sudo apt -y install apache2
 sudo apt -y install php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
-sudo systemctl restart apache2
-sudo curl -o /var/www/html/latest.tar.gz https://wordpress.org/latest.tar.gz
-sudo tar -xvf /var/www/html/latest.tar.gz -C /var/www/html
-sudo rm /var/www/html/latest.tar.gz
-sudo cat install_script_bap/wp-config.php > /var/www/html/wordpress/wp-config.php
+sudo tar -xvf install_script_bap/latest.tar.gz -C /var/www/html
 sudo chown -R www-data:www-data /var/www/html/wordpress
 sudo find /var/www/html/wordpress/ -type d -exec chmod 750 {} \;
 sudo find /var/www/html/wordpress/ -type f -exec chmod 640 {} \;
